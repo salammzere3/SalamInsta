93fedd2dbfcaa2f89ec437a87f574a634fa6fab2
Salam Mzere <81921816+salammzere3@users.noreply.github.com>
2022-03-16 03:21:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,54 @@
-import setuptools
+import os , codecs
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
 
-setuptools.setup(
+pkg = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(pkg, "README.md"), encoding="utf-8") as fh:
+	
+    long_description = "\n" + fh.read()
+
+
+setup(
+
     name="SalamInsta",
+    
     version="1.0.1",
+    
     author="salammzere3",
+    
     author_email="salam123@hi2.in",
-    description="package login instagram",
-    long_description=long_description,
+    
+    description = ("login Instagram"),
+    
     long_description_content_type="text/markdown",
+    
     url="https://github.com/salammzere3/SalamInsta",
+    
     project_urls={
         "Bug Tracker": "https://github.com/salammzere3/SalamInsta/issues",
     },
+    
+    long_description=long_description,
+    
+    packages=find_packages(),
+    
+    install_requires=['requests'],
+    
+    keywords=['instagram'],
+    
     classifiers=[
+    
+        "Development Status :: 1 - Planning",
+        
+        "Intended Audience :: Developers",
+        
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+        
+        "Operating System :: Unix",
+        
+        "Operating System :: MacOS :: MacOS X",
+        
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
